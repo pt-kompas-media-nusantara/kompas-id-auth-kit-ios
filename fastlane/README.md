50020074982464c2888a7f922e5556966b3e9f06
nurirppan <nurirppan@gmail.com>
2025-11-21 18:48:58 +0700

    ! add bundle exec fastlane exec_delete_bad_release

--- fastlane/README.md
+++ fastlane/README.md
@@ -47,6 +47,14 @@ fastlane exec_clear_cache_pods
 
 bundle exec fastlane exec_create_tag_release
 
+### ios exec_delete_bad_releaseOne
+
+```sh
+[bundle exec] fastlane ios exec_delete_bad_releaseOne
+```
+
+bundle exec fastlane exec_delete_bad_releaseOne
+
 ### ios exec_delete_bad_release
 
 ```sh
--- fastlane/exec_remove_git_tag.rb
+++ fastlane/exec_remove_git_tag.rb
@@ -3,8 +3,8 @@ default_platform(:ios)
 platform :ios do
 
   # Hapus Tag di Local dan Remote
-  desc "bundle exec fastlane exec_delete_bad_release"
-  lane :exec_delete_bad_release do
+  desc "bundle exec fastlane exec_delete_bad_releaseOne"
+  lane :exec_delete_bad_releaseOne do
 
     tag_to_delete = UI.input("Tag Version")
         
@@ -23,6 +23,39 @@ platform :ios do
     else
       UI.message("Oke, batal hapus.")
     end
+  end
+
+  # Hapus Tag di Local dan Remote (Manual Shell)
+  desc "bundle exec fastlane exec_delete_bad_release"
+  lane :exec_delete_bad_release do
+
+    version = UI.input("Tag Version")
+    
+    unless version
+      UI.user_error!("Wajib masukin versi bro! Contoh: fastlane delete_bad_release version:1.0.1")
+    end
+
+    tag_to_delete = version
+    
+    UI.important("⚠️  Warning: Lu akan menghapus tag #{tag_to_delete} di Local & Remote!")
+      # Kita pakai 'sh' untuk menjalankan command git biasa
+      # Kita pakai begin/rescue biar kalau tagnya gak ada di lokal, fastlane gak crash
+      begin
+        sh("git tag -d #{tag_to_delete}")
+        UI.success("✅ Tag Lokal #{tag_to_delete} terhapus.")
+      rescue => ex
+        UI.important("⚠️  Tag Lokal #{tag_to_delete} gak ketemu atau udah hapus.")
+      end
+
+      # 3. Hapus Tag Remote (GitHub)
+      begin
+        sh("git push origin --delete #{tag_to_delete}")
+        UI.success("✅ Tag Remote #{tag_to_delete} terhapus.")
+      rescue => ex
+        UI.important("⚠️  Tag Remote #{tag_to_delete} gak ketemu atau udah hapus.")
+      end
+      
+      UI.success("Selesai bersih-bersih! 🗑️")
   end
 
 end
