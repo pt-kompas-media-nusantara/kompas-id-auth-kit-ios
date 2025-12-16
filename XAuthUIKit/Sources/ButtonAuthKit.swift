28fab68ed46594ec4495043a63d00c6615efbc0a
nurirppan <nurirppan@gmail.com>
2025-12-16 17:09:48 +0700

    ! checkpoint setup flavor masih gagal

--- XAuthUIKit/Sources/ButtonAuthKit.swift
+++ XAuthUIKit/Sources/ButtonAuthKit.swift
@@ -3,11 +3,53 @@ import SwiftUI
 
 public struct ButtonAuthKit: View {
     
+    var environmentName: String {
+#if STAGING
+        return "STAGING"
+#elseif PRODUCTION
+        return "PRODUCTION"
+#else
+        return "UNKNOWN environmentName"
+#endif
+    }
+    
+    var modeName: String {
+#if DEBUG
+        return "DEBUG"
+#elseif RELEASE
+        return "RELEASE"
+#else
+        return "UNKNOWN modeName"
+#endif
+    }
+    
     // 1. Initializer-nya WAJIB public
     public init() {}
     
     // 2. Body-nya WAJIB public
     public var body: some View {
-        Text("ButtonAuthKit NIH")
+        Button {
+            print("")
+        } label: {
+            VStack {
+                Text("\(environmentName) - \(modeName)")
+                
+#if PRODUCTION_DEBUG
+                Text("PRODUCTION_DEBUG")
+#elseif PRODUCTION_RELEASE
+                Text("#elseif PRODUCTION_RELEASE")
+#elseif STAGING_DEBUG
+                Text("STAGING_DEBUG")
+#elseif STAGING_RELEASE
+                Text("STAGING_RELEASE")
+#else
+                Text("UNKNOWN LANGSUNG")
+#endif
+            }
+        }
+        
     }
 }
+
+
+
